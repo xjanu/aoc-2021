72f8c7dee9b32dae4ef3e8f1e5d571afec985e1f
Martin Janů <martin.janu@protonmail.com>
2021-12-01 12:12:36 +0100

    Add arg for part 2

--- day01/Depth.hs
+++ day01/Depth.hs
@@ -2,7 +2,11 @@ import System.Environment
 import System.IO
 
 usage :: String -> String
-usage progname = "Usage: " ++ progname ++ " <filename>\n"
+usage progname =
+    "Usage: " ++ progname ++ " [<part>] <filename>\n" ++
+    "Where <part> is either 1 or 2, depending on the part of the challenge,\n" ++
+    "             default value is 1.\n" ++
+    "      <filename> is the path to the puzzle input file.\n"
 
 -- Count the number of increases
 count :: [Integer] -> Integer
@@ -10,19 +14,31 @@ count ls =
     fst $ foldl fun (0, head ls) ls
     where fun (count, prev) next = (count + if next > prev then 1 else 0, next)
 
+-- Calculate the sliding window
+sliding :: [Integer] -> [Integer]
+sliding _ = [] -- TODO
+
 -- Read file and process input
-depth :: FilePath -> IO ()
-depth file = do
+depth :: Int -> FilePath -> IO ()
+depth part file = do
     raw <- readFile file
     let nums = read `map` lines raw :: [Integer]
-    print $ count nums
+    if part == 2
+    then print $ count $ sliding nums
+    else print $ count nums
 
 -- Parse args
+parseArgs :: [String] -> (Int, FilePath)
+parseArgs [part, file] = (read part, file)
+parseArgs [file]       = (1, file)
+
 main :: IO ()
 main = do
     args <- getArgs
-    if length args /= 1
+    if null args || length args > 2
     then getProgName >>= hPutStr stderr . usage
     else do
-        let file = head args
-        depth file
+        let (part, file) = parseArgs args
+        if part /= 1 && part /= 2
+        then getProgName >>= hPutStr stderr . usage
+        else depth part file
