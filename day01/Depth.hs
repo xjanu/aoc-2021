b80ad89e044a5deb2649f1aee8f7e9ebb4556e8a
Martin Janů <martin.janu@protonmail.com>
2021-12-01 11:47:41 +0100

    Add documenting comments

--- day01/Depth.hs
+++ day01/Depth.hs
@@ -4,17 +4,20 @@ import System.IO
 usage :: String -> String
 usage progname = "Usage: " ++ progname ++ " <filename>\n"
 
+-- Count the number of increases
 count :: [Integer] -> Integer
 count ls =
     fst $ foldl fun (0, head ls) ls
     where fun (count, prev) next = (count + if next > prev then 1 else 0, next)
 
+-- Read file and process input
 depth :: FilePath -> IO ()
 depth file = do
     raw <- readFile file
     let nums = read `map` lines raw :: [Integer]
     print $ count nums
 
+-- Parse args
 main :: IO ()
 main = do
     args <- getArgs
