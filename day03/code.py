5cf6587b281c9c14ed72f6ba9a209fb843406c03
Martin Janů <martin.janu@protonmail.com>
2021-12-03 08:17:25 +0100

    Document code

--- day03/code.py
+++ day03/code.py
@@ -7,37 +7,41 @@ Where <part> is either 1 or 2, depending on the part of the challenge,
              default value is 1.
       <filename> is the path to the puzzle input file."""
 
+# Return a list of lines
 def parse_input(file):
     with open(file, "r") as f:
         lines = f.read().splitlines()
     return lines
 
+# Return the most- and least-common bits in each column of input
 def count_bits(lines):
     if len(lines) == 0:
         return "", ""
 
-    count =  [0 for i in range(len(lines[0]))]
+    count1 = [0 for i in range(len(lines[0]))]
     count0 = count.copy()
 
     for line in lines:
         for i in range(len(count)):
-            count[i] += 1 if line[i] == '1' else 0
+            count1[i] += 1 if line[i] == '1' else 0
             count0[i] += 1 if line[i] == '0' else 0
 
     mcb = ""
     lcb = ""
-    for i in range(len(count)):
-        mcb += "1" if count[i] >= count0[i] else "0"
-        lcb += "1" if count[i] <  count0[i] else "0"
+    for i in range(len(count1)):
+        mcb += "1" if count1[i] >= count0[i] else "0"
+        lcb += "1" if count1[i] <  count0[i] else "0"
 
     return mcb, lcb
 
+# First part
 def part_1(lines):
     mcb, lcb = count_bits(lines)
     gamma = int(mcb, base=2)
     epsilon = int(lcb, base=2)
     print(gamma * epsilon)
 
+# Second part
 def part_2(lines):
     oxy_lines = lines.copy()
     co2_lines = lines.copy()
@@ -57,6 +61,7 @@ def part_2(lines):
     print(oxy * co2)
 
 if __name__ == "__main__":
+    # Parse arguments
     part = 1
     file = argv[-1]
     if len(argv) <= 1 or len(argv) > 3:
@@ -64,8 +69,10 @@ if __name__ == "__main__":
     elif len(argv) == 3:
         part = int(argv[1])
 
+    # Read file
     lines = parse_input(file)
 
+    # solution.run()
     if part == 1:
         part_1(lines)
     elif part == 2:
