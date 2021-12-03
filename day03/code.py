5b771d52b14fb80882d48c7ff0e70923ab24c5a3
Martin Janů <martin.janu@protonmail.com>
2021-12-03 08:03:53 +0100

    Refactor

--- day03/code.py
+++ day03/code.py
@@ -7,14 +7,30 @@ Where <part> is either 1 or 2, depending on the part of the challenge,
              default value is 1.
       <filename> is the path to the puzzle input file."""
 
-def parse_line(line):
-    # TODO
-    return line
-
 def parse_input(file):
     with open(file, "r") as f:
         lines = f.read().splitlines()
-    return list((map(parse_line, lines)))
+    return lines
+
+def count_bits(lines):
+    if len(lines) == 0:
+        return "", ""
+
+    count =  [0 for i in range(len(lines[0]))]
+    count0 = count.copy()
+
+    for line in lines:
+        for i in range(len(count)):
+            count[i] += 1 if line[i] == '1' else 0
+            count0[i] += 1 if line[i] == '0' else 0
+
+    mcb = ""
+    lcb = ""
+    for i in range(len(count)):
+        mcb += "1" if count[i] >= count0[i] else "0"
+        lcb += "1" if count[i] <  count0[i] else "0"
+
+    return mcb, lcb
 
 if __name__ == "__main__":
     part = 1
@@ -28,52 +44,26 @@ if __name__ == "__main__":
             exit(2)
 
     lines = parse_input(file)
-    count = [0 for i in range(len(lines[0]))]
-    count0 = [0 for i in range(len(lines[0]))]
-    for line in lines:
-        for i in range(len(count)):
-            count[i] += 1 if line[i] == '1' else 0
-            count0[i] += 1 if line[i] == '0' else 0
-    mcb = ""
-    lcb = ""
-    for i in range(len(count)):
-        mcb += "1" if count[i] >= count0[i] else "0"
-        lcb += "1" if count[i] <  count0[i] else "0"
-    gamma = int("".join(list(map(str, mcb))), base=2)
-    epsilon = int("".join(list(map(str, lcb))), base=2)
+    mcb, lcb = count_bits(lines)
+    gamma = int(mcb, base=2)
+    epsilon = int(lcb, base=2)
 
     if part == 1:
-        print(gamma, epsilon, gamma * epsilon)
+        print(gamma * epsilon)
         exit(0)
 
-    oxygen = lines.copy()
-    co2    = lines.copy()
-    for j in range(len(count)):
-        count = [0 for i in range(len(lines[0]))]
-        count0 = [0 for i in range(len(lines[0]))]
-        for line in oxygen:
-            for i in range(len(count)):
-                count[i] += 1 if line[i] == '1' else 0
-                count0[i] += 1 if line[i] == '0' else 0
-        mcb = ""
-        for i in range(len(count)):
-            mcb += "1" if count[i] >= count0[i] else "0"
-        count = [0 for i in range(len(lines[0]))]
-        count0 = [0 for i in range(len(lines[0]))]
-        for line in co2:
-            for i in range(len(count)):
-                count[i] += 1 if line[i] == '1' else 0
-                count0[i] += 1 if line[i] == '0' else 0
-        lcb = ""
-        for i in range(len(count)):
-            lcb += "1" if count[i] <  count0[i] else "0"
+    oxy_lines = lines.copy()
+    co2_lines = lines.copy()
+    for bit in range(len(lines[0])):
+        mcb, _ = count_bits(oxy_lines)
+        _, lcb = count_bits(co2_lines)
 
-        oxygen = [val for val in oxygen if val[j] == mcb[j]]
-        co2    = [val for val in co2    if val[j] == lcb[j]]
-        print(oxygen)
-        if len(oxygen) == 1:
-            oxy = int(oxygen[0], base=2)
-        if len(co2) == 1:
-            co = int(co2[0], base=2)
+        oxy_lines = [val for val in oxy_lines if val[bit] == mcb[bit]]
+        co2_lines    = [val for val in co2_lines    if val[bit] == lcb[bit]]
 
-    print(oxy, co, oxy * co)
+        if len(oxy_lines) == 1:
+            oxy = int(oxy_lines[0], base=2)
+        if len(co2_lines) == 1:
+            co2 = int(co2_lines[0], base=2)
+
+    print(oxy * co2)
