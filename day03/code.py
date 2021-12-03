cc8c9078a0da9c8e4b6f7b95b0b096dd076465e1
Martin Janů <martin.janu@protonmail.com>
2021-12-03 06:51:30 +0100

    Day03: Second part

--- day03/code.py
+++ day03/code.py
@@ -27,18 +27,53 @@ if __name__ == "__main__":
             print(usage, file=stderr)
             exit(2)
 
-    line = parse_input(file)[0]
-    count = [0 for i in range(len(line))]
-    count0 = [0 for i in range(len(line))]
-    for line in parse_input(file):
+    lines = parse_input(file)
+    count = [0 for i in range(len(lines[0]))]
+    count0 = [0 for i in range(len(lines[0]))]
+    for line in lines:
         for i in range(len(count)):
             count[i] += 1 if line[i] == '1' else 0
             count0[i] += 1 if line[i] == '0' else 0
     mcb = ""
     lcb = ""
     for i in range(len(count)):
-        mcb += "1" if count[i] > count0[i] else "0"
-        lcb += "1" if count[i] < count0[i] else "0"
+        mcb += "1" if count[i] >= count0[i] else "0"
+        lcb += "1" if count[i] <  count0[i] else "0"
     gamma = int("".join(list(map(str, mcb))), base=2)
     epsilon = int("".join(list(map(str, lcb))), base=2)
-    print(gamma, epsilon, gamma * epsilon)
+
+    if part == 1:
+        print(gamma, epsilon, gamma * epsilon)
+        exit(0)
+
+    oxygen = lines.copy()
+    co2    = lines.copy()
+    for j in range(len(count)):
+        count = [0 for i in range(len(lines[0]))]
+        count0 = [0 for i in range(len(lines[0]))]
+        for line in oxygen:
+            for i in range(len(count)):
+                count[i] += 1 if line[i] == '1' else 0
+                count0[i] += 1 if line[i] == '0' else 0
+        mcb = ""
+        for i in range(len(count)):
+            mcb += "1" if count[i] >= count0[i] else "0"
+        count = [0 for i in range(len(lines[0]))]
+        count0 = [0 for i in range(len(lines[0]))]
+        for line in co2:
+            for i in range(len(count)):
+                count[i] += 1 if line[i] == '1' else 0
+                count0[i] += 1 if line[i] == '0' else 0
+        lcb = ""
+        for i in range(len(count)):
+            lcb += "1" if count[i] <  count0[i] else "0"
+
+        oxygen = [val for val in oxygen if val[j] == mcb[j]]
+        co2    = [val for val in co2    if val[j] == lcb[j]]
+        print(oxygen)
+        if len(oxygen) == 1:
+            oxy = int(oxygen[0], base=2)
+        if len(co2) == 1:
+            co = int(co2[0], base=2)
+
+    print(oxy, co, oxy * co)
