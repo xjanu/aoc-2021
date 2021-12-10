8b5f2ff4652264f3d32f5a0de3316c1af09e30f3
Martin Janů <martin.janu@protonmail.com>
2021-12-10 19:39:58 +0100

    Day 09: Second part

--- day09/smoke.py
+++ day09/smoke.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-with open("test", "r") as f:
+from math import prod
+
+with open("input", "r") as f:
     data = list(map(lambda x: list(map(int, x.strip())), f.readlines()))
 
 def prev_neighbors(x,y):
@@ -9,33 +11,29 @@ def prev_neighbors(x,y):
     if y > 0:
         yield x, y - 1
 
-def next_neighbors(x,y):
-    if x < len(data[0]) - 1:
-        yield x + 1, y
-    if y < len(data) - 1:
-        yield x, y + 1
+basins = [[(x, y)] for x in range(len(data[0])) for y in  range(len(data))]
 
-basins = []
+def find_basin(x, y):
+    for i in range(len(basins)):
+        if (x, y) in basins[i]:
+            return i
 
-point_to_basin = {}
-
-s = 0
-for y in range(len(data)):
-    for x in range(len(data[0])):
+for x in range(len(data[0])):
+    for y in range(len(data)):
         if data[y][x] == 9:
+            basins.remove([(x, y)])
             continue
         for nx, ny in prev_neighbors(x, y):
-            print(x,y, nx,ny, data[y][x], data[ny][nx])
-            if (data[y][x] >= data[ny][nx] or True) and data[ny][nx] != 9:
-                basins[point_to_basin[(nx, ny)]] += 1
-                point_to_basin[(x,y)] = point_to_basin[(nx, ny)]
-        #for nx, ny in next_neighbors(x, y):
-        if (data[y][x] < data[ny][nx] or True) and (x, y) not in point_to_basin.keys():
-            basins.append(1)
-            point_to_basin[(x,y)] = len(basins) - 1
-        else:
-            s += data[y][x] + 1
+            if data[ny][nx] == 9:
+                continue
+            b_1 = find_basin(x, y)
+            b_2 = find_basin(nx, ny)
+            if b_1 == b_2:
+                continue
+            basins[b_1].extend(basins[b_2])
+            basins.pop(b_2)
 
+basins = list(map(len, basins))
 basins.sort(reverse=True)
 
-print(basins, point_to_basin)
+print(basins[:3], prod(basins[:3]))
